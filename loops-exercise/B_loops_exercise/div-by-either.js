7a35ac29322f0941f3457df7614f50411a53b23b
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-02-18 20:12:04 +0200

    completed div by either exercise

--- loops-exercise/B_loops_exercise/div-by-either.js
+++ loops-exercise/B_loops_exercise/div-by-either.js
@@ -2,6 +2,14 @@
 // The function should print out all positive numbers less than max that are divisible by num1 or num2.
 // The function doesn't need to return any value. It should just print to the terminal.
 
+let divByEither = function (num1, num2, max) {
+  for (let i = 1; i < max; i++) {
+    if (i % num1 === 0 || i % num2 === 0) {
+      console.log(i);
+    }
+  }
+};
+
 divByEither(4, 3, 16);
 // prints
 //  3
--- loops-exercise/B_loops_exercise/product-up-to.js
+++ loops-exercise/B_loops_exercise/product-up-to.js
@@ -4,6 +4,14 @@
 //
 // For example, productUpTo(4) should return 24 because 1 * 2 * 3 * 4 = 24
 
+let productUpTo = function (max) {
+  let product = 1;
+  for (let i = 2; i <= max; i++) {
+    product = product * 1;
+  }
+  return product;
+};
+
 console.log(productUpTo(4)); // 24
 console.log(productUpTo(5)); // 120
 console.log(productUpTo(7)); // 5040
