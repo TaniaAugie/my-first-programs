d68192e78cb9d1773c99df2a860177eb0b37a6a1
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-02-19 21:18:35 +0200

    completed fizz buzz exercise

--- 03_loops/C_loops_exercise/fizz-buzz.js
+++ 03_loops/C_loops_exercise/fizz-buzz.js
@@ -2,6 +2,14 @@
 // print all numbers less than or equal to max that are divisible by either 3 or 5 but not both 3
 // and 5. The function doesn't need to return any value. It should just print to the terminal.
 
+let fizzBuzz = function (max) {
+  for (let i = 1; i <= max; i++) {
+    if (i % 3 === 0 || (i % 5 == 0 && !(i % 3 == 0 && i % 5 == 0))) {
+      console.log(i);
+    }
+  }
+};
+
 fizzBuzz(18);
 // prints
 //  3
