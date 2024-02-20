6b87ced4eef295682ab0b0193ed028c99856c598
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-02-20 10:52:43 +0200

    renamed folders

--- loops-exercise/C_loops_exercise/remove-capital.js
+++
@@ -1,6 +0,0 @@
-// Write a function `removeCapitals` that accepts a string as an argument. The function should return a
-// new version of the string with all capital letters removed.
-
-console.log(removeCapitals("fOrEver")); // 'frver'
-console.log(removeCapitals("raiNCoat")); // 'raioat'
-console.log(removeCapitals("cElLAr Door")); // 'clr oor'
