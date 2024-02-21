6e66bd160f26a9439e3efe1f283e348aabd59868
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-02-21 11:44:38 +0200

    completed common elements exercise

--- 04_arrays/C_arrays_exercise/common-elements.js
+++ 04_arrays/C_arrays_exercise/common-elements.js
@@ -2,6 +2,19 @@
 // a new array containing the elements that are found in both of the input arrays. The order of
 // the elements in the output array doesn't matter as long as the function returns the correct elements.
 
+let commonElements = function (array1, array2) {
+  let common = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    let ele = array1[i];
+
+    if (array2.includes(ele)) {
+      common.push(ele);
+    }
+  }
+  return common;
+};
+
 let arr1 = ["a", "c", "d", "b"];
 let arr2 = ["b", "a", "y"];
 console.log(commonElements(arr1, arr2)); // ['a', 'b']
