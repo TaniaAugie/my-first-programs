df94028a868f5ce566987361008b4c1cf8de78f6
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-02-21 14:19:40 +0200

    completed two sum exercise

--- 05_nesting/A_nested_loops_exercise/two-sum.js
+++ 05_nesting/A_nested_loops_exercise/two-sum.js
@@ -2,6 +2,17 @@
 // as an argument. The function should return a boolean indicating whether or not there exists a pair
 // of distinct elements in the array that sum to the target.
 
+let twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
 console.log(twoSum([2, 3, 5, 9], 7)); // true
 console.log(twoSum([2, 3, 5, 9], 4)); // false
 console.log(twoSum([6, 3, 4], 10)); // true
