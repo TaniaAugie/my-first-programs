611090bf0e86bd8bd811fb12577ec973933bc934
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-04 13:53:25 +0200

    exercise1

---
+++ ES6/destructuring/exercise2.js
@@ -0,0 +1,9 @@
+// Refactor the following code to use object destructuring assignment.
+
+/* function addressMaker(address) {
+  const newAddress = {
+    city: address.city,
+    state: address.state,
+    country: "United States",
+  };
+} */
