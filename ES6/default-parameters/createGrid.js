6f9f8215b58ed4567cd97dcc4b87c64067413e20
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-04 13:23:03 +0200

    create grid

--- ES6/default-parameters/createGrid.js
+++ ES6/default-parameters/createGrid.js
@@ -1,1 +1,11 @@
+// Refactor the function createGrid() to use the destructuring.
 
+/* function createGrid(array) {
+  const width = array[0];
+  const height = array[1];
+  return `Generates a ${width} x ${height} grid`;
+} */
+
+function createGrid([width, height]) {
+  return `Generates a ${width} x ${height} grid`;
+}
