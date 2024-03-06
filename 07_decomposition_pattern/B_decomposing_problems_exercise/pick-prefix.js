6f8568d9ce555455085b52929cb901d6bf5833d0
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-06 12:03:41 +0200

    completed pick prefix exercise

--- 07_decomposition_pattern/B_decomposing_problems_exercise/pick-prefix.js
+++ 07_decomposition_pattern/B_decomposing_problems_exercise/pick-prefix.js
@@ -1,6 +1,17 @@
 // Write a function `pickPrefix(strings, prefix)` that accepts an array of strings and a prefix string
 // as an argument. The function should return the an array of words that begin with the prefix.
 
+const pickPrefix = function (arr, str) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+    if (word.includes(str)) {
+      newArr.push(word);
+    }
+  }
+  return newArr;
+};
+
 console.log(pickPrefix(["connect", "company", "concert", "cram"], "con"));
 // ['connect', 'concert']
 
