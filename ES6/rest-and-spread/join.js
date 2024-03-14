5f269e7c011bf01b244ed356b24f90aae2abc416
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-14 09:47:57 +0200

    added html

--- ES6/rest-and-spread/join.js
+++
@@ -1,9 +0,0 @@
-// Refactor the function join() to use the spread operator.
-
-/* function join(array1, array2) {
-  return array1.concat(array2);
-} */
-
-function join(array1, array2) {
-  return [...array1, ...array2];
-}
