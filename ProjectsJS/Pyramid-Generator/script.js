69ab8b59afab1bcb801503d8ab30ef566afd0f82
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-22 11:38:58 +0200

    done

--- ProjectsJS/Pyramid-Generator/script.js
+++ ProjectsJS/Pyramid-Generator/script.js
@@ -6,9 +6,7 @@ function generatePyramid() {
     for (let spaceNum = 1; spaceNum <= rows - rowNum; spaceNum++) {
       rowHtml += '<div class="block"></div>';
     }
-    for (let starNum = 1; starNum <= 2 * rowNum - 1; starNum++) {
-      rowHtml += '<div class="block">*</div>';
-    }
+
     pyramidHtml += rowHtml + "<br>";
   }
   document.getElementById("pyramidOutput").innerHTML = pyramidHtml;
