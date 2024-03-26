138ca988c2a9e4b237771059f5be094d5747c884
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2024-03-26 09:55:34 +0200

    nextjs

--- 00_expressions_variables/B_Expessions_exercise/string-expressions.js
+++
@@ -1,5 +0,0 @@
-console.log("river" + "town"); // rivertown
-console.log("cat" + "dog"); // catdog
-console.log("New" + "York"); // NewYork
-console.log("runner's knee" + "!"); // runner's knee!
-console.log("man" + "bear" + "pig"); // manbearpig
